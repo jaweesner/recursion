649ace28705a58ce3e5dfda075f11f0b3279b6eb
Jane Weesner <jane.e.weesner@gmail.com>
2018-02-13 15:17:24 -0600

    Completed getElementsByClassName.ja

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var matchingElements = [];
+
+  recursiveFunc(document.body);
+
+  return matchingElements;
+
+
+  //recursive helper function. uses sideEffects, not sure if I want to refactor....
+  function recursiveFunc(element) {
+    if (_.contains(element.classList, className)) {
+      matchingElements.push(element);
+    }
+
+    //base case when there are no child nodes, just return
+    if (element.childNodes.length === 0) {
+      return;
+      //if there are child nodes recursively look at each child and add each as needed  
+    } else {
+      _.each(element.childNodes, recursiveFunc);
+    }
+  };
+
+
 };
