acc4d35dfccff324523cfa722a14e08f87cbdd74
Jane Weesner <jane.e.weesner@gmail.com>
2018-02-12 18:35:01 -0600

    stringify complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,37 @@
 
 // but you don't so you're going to write it from scratch:
 
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj==='undefined' || typeof obj === 'function'){
+    return;
+
+  }else if ( typeof obj === "string" ){
+    return `"${obj}"`;
+
+  }else if(obj===null || typeof obj !== "object"){
+    return `${obj}`;
+
+
+
+  }else if (Array.isArray(obj)){
+    return `[${_.map(obj,stringifyJSON)}]`;
+  }
+  else{
+    var internalString = _.reduce(obj, function(fullString, val, key){
+      if (typeof val === 'undefined' || typeof val === 'function'){
+        return "";
+      }
+      return `${fullString},"${key}":${stringifyJSON(val)}`;
+    },"");
+    if (internalString){
+     internalString = internalString.slice(1);
+    }
+    return `{${internalString}}`;
+        
+  };
+  
 };
+
+
